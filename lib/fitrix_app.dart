35d68dff011fdc4b0fa8ac82f0836e709d86477e
ahmedbelal <ahmedbelal1.exe@gmail.com>
2025-09-16 03:42:28 +0300

    feat: initialize app with DevicePreview, ScreenUtil, and basic routing
    This commit introduces initial app setup, including:
    
    - Integration of `DevicePreview` for responsive UI testing (disabled by default).
    - Configuration of `ScreenUtil` for adaptive screen sizing.
    - Basic routing setup with `AppRouter`.
    - Localization setup using `flutter_localizations` and generated `S` delegate.
    - Addition of Arabic and English localization files.
    - Configuration of `MyBlocObserver` for Bloc state management.
    - Service locator setup using `get_it`.
    - Removal of placeholder `MyHomePage` and related code.
    - Creation of `FitrixApp` widget as the new root of the application.
    - Added `Cairo` Google Font as the default font family.
    - Set primary color and scaffold background color in the theme.

---
+++ lib/fitrix_app.dart
@@ -0,0 +1,53 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'core/di/get_it.dart';
+import 'core/routing/app_router.dart';
+import 'core/routing/routes.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'core/theming/app_colors.dart';
+import 'generated/l10n.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<ScaffoldMessengerState> messengerKey =
+    GlobalKey<ScaffoldMessengerState>();
+
+class FitrixApp extends StatelessWidget {
+  final AppRouter appRouter;
+
+  const FitrixApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(412, 917),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        navigatorKey: navigatorKey,
+        useInheritedMediaQuery: true,
+        builder: DevicePreview.appBuilder,
+        title: 'FitrixApp',
+        theme: ThemeData(
+          primaryColor: ColorsManager.primaryGreen,
+          scaffoldBackgroundColor: Colors.white,
+          fontFamily: GoogleFonts.cairo().fontFamily,
+        ),
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: appRouter.generateRoute,
+        initialRoute: Routes.splashScreen,
+      ),
+    );
+  }
+}
